88092f7b973f06e1b161bfca3de323ca80f22e93
Kader Bozkaya <87239102+kaderbozkaya@users.noreply.github.com>
2024-12-30 14:18:51 +0300

    read blog post

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <div>HOMEEEEE</div>;
+import PostCard from "@/components/PostCard";
+import { getCollection } from "@/lib/db";
+
+export default async function Home() {
+  //veri tabanından blogları okumak için koleksiyon oluşturuyoruz
+  const postCollection = await getCollection("posts");
+  //tüm postları almak için bir find yöntemi kullanıyoruz.nesneler üzerinde dönmesi için array olarak alıyoruz
+  const posts = await postCollection?.find().sort({ $natural: -1 }).toArray(); //mongodbye ait olan natural ile son eklenen ilk başa geliyor.yani tersine sıralanıyor
+  if (posts) {
+    return (
+      <div className="grid grid-cols-2 gap-6">
+        {posts.map((post) => (
+          <div key={post._id}>
+            <PostCard post={post} />
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    return <p>Failed to fetch the data from database.</p>;
+  }
 }
---
+++ src/components/PostCard.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+export default function PostCard({ post }) {
+  return (
+    <div className="border border-slate-400 border-dashed p-4 rounded-md h-full">
+      <p className="text-slate-600 text-xs">
+        {post._id.getTimestamp().toLocaleString()}
+      </p>
+      {/*mongodbnin objectid türünden bir belge kimliği olan _id için oluşturulma tarihini alıp bu tarihi yerel bir tarih saat biçimine dönüştürme */}
+      <Link href="" className="block text-xl font-semibold mb-4">
+        {post.title}
+      </Link>
+      <p className="text-sm">{post.content}</p>
+    </div>
+  );
+}
